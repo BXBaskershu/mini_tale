61ee33b780dc57be5bad20c9c862a672872b741c
BXBaskerShu <yangshuzhi@baixing.com>
2019-04-08 15:05:19 +0800

    更改电话号码的校验规则

--- lib/validators.py
+++ lib/validators.py
@@ -3,10 +3,11 @@ import re
 
 class Validator:
 
+    MOBILE_PATTERN = r'1[2-9]\d{9}'
     PHONE_PATTERN = r'0\d+'
 
     @staticmethod
     def validate_phone(val):
-        """ 检验传入的数据是否是一个电话号码 """
-        phone_regex = re.compile(Validator.PHONE_PATTERN)
-        return phone_regex.match(val) and len(val) >= 6 and len(val) <= 13
+        """ 检验传入的数据是否是一个手机号码 """
+        phone_regex = re.compile(Validator.MOBILE_PATTERN)
+        return phone_regex.match(val) and len(val) == 11
